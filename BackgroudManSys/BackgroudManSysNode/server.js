d96868deb76e6985adf9510a0da360faeba4ad65
Liuxin0505 <819238426@qq.com>
2018-09-25 17:26:21 +0800

    配置mblab

--- BackgroudManSys/BackgroudManSysNode/server.js
+++ BackgroudManSys/BackgroudManSysNode/server.js
@@ -1,6 +1,18 @@
 const express=require("express");
+const mongoose=require("mongoose");
 const app=express();
-// app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Hello Worlds!'));
+
+//DB config
+const db=require("./config/key").mongoURI;
+//connect mongdb
+mongoose.connect(db)
+    .then(()=>{
+        console.log("connect db successful")
+    })
+    .catch(()=>{
+        console.log("connect db failed")
+    })
 
 const port=process.env.PORT || 5000
 app.listen(port,()=>{
